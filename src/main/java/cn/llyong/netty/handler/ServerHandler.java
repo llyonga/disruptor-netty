0e9a553b57407bc86da5abb15801e1d52c70af9d
lvyong <lvyongfh@163.com>
2019-09-05 15:55:33 +0800

    init

---
+++ src/main/java/cn/llyong/netty/handler/ServerHandler.java
@@ -0,0 +1,15 @@
+package cn.llyong.netty.handler;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @description:
+ * @author: lvyong
+ * @date: 2019-09-05
+ * @time: 3:54 下午
+ * @version: 1.0
+ */
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+}
